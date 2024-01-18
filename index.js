7feabab1900b20db5aa1919bea38f7f132571e7b
Alpaoca <preechak10123@gmail.com>
2024-01-18 15:32:45 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const faqsQuestionsBox = document.querySelectorAll(".faqs-question");
+
+faqsQuestionsBox.forEach((box, index) => {
+  box.addEventListener("click", () => {
+    var faqsDescription = document.getElementsByClassName(
+      "faqs-question-description"
+    );
+    var faqsIcon = document.getElementsByClassName("faqs-icon");
+    if (faqsDescription[index].style.display === "none") {
+      faqsDescription[index].style.display = "inline";
+      faqsIcon[index].src = "/assets/images/icon-minus.svg";
+    } else {
+      faqsDescription[index].style.display = "none";
+      faqsIcon[index].src = "/assets/images/icon-plus.svg";
+    }
+  });
+});
