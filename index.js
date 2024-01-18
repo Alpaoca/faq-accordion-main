d64f9518d36a9c20be8de7327496f5fb73bd6ef9
Alpaoca <preechak10123@gmail.com>
2024-01-18 15:51:10 +0700

    Fix relative path in css & js

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ faqsQuestionsBox.forEach((box, index) => {
     var faqsIcon = document.getElementsByClassName("faqs-icon");
     if (faqsDescription[index].style.display === "none") {
       faqsDescription[index].style.display = "inline";
-      faqsIcon[index].src = "/assets/images/icon-minus.svg";
+      faqsIcon[index].src = "./assets/images/icon-minus.svg";
     } else {
       faqsDescription[index].style.display = "none";
-      faqsIcon[index].src = "/assets/images/icon-plus.svg";
+      faqsIcon[index].src = "./assets/images/icon-plus.svg";
     }
   });
 });
